185c9f14e9a067dc6ef1aab0f160af629413102c
shawn <sdd031215@163.com>
2019-07-08 13:03:51 +0800

    Create Viterbi.java

---
+++ shawn/Viterbi.java
@@ -0,0 +1,106 @@
+
+/*
+def viterbi(obs, states, start_p, trans_p, emit_p):
+    """
+ 
+    :param obs:观测序列
+    :param states:隐状态
+    :param start_p:初始概率（隐状态）
+    :param trans_p:转移概率（隐状态）
+    :param emit_p: 发射概率 （隐状态表现为显状态的概率）
+    :return:
+    """
+    # 路径概率表 V[时间][隐状态] = 概率
+    V = [{}]
+    # 一个中间变量，代表当前状态是哪个隐状态
+    path = {}
+ 
+    # 初始化初始状态 (t == 0)
+    for y in states:
+        V[0][y] = start_p[y] * emit_p[y][obs[0]]
+        path[y] = [y]
+ 
+    # 对 t > 0 跑一遍维特比算法
+    for t in range(1, len(obs)):
+        V.append({})
+        newpath = {}
+ 
+        for y in states:
+            # 概率 隐状态 =    前状态是y0的概率 * y0转移到y的概率 * y表现为当前状态的概率
+            (prob, state) = max([(V[t - 1][y0] * trans_p[y0][y] * emit_p[y][obs[t]], y0) for y0 in states])
+            # 记录最大概率
+            V[t][y] = prob
+            # 记录路径
+            newpath[y] = path[state] + [y]
+ 
+        # 不需要保留旧路径
+        path = newpath
+ 
+    print_dptable(V)
+    (prob, state) = max([(V[len(obs) - 1][y], y) for y in states])
+    return (prob, path[state])
+*/
+
+public class Viterbi
+{
+    /**
+     * 求解HMM模型
+     * @param obs 观测序列
+     * @param states 隐状态
+     * @param start_p 初始概率（隐状态）
+     * @param trans_p 转移概率（隐状态）
+     * @param emit_p 发射概率 （隐状态表现为显状态的概率）
+     * @return 最可能的序列
+     */
+    public static int[] compute(int[] obs, int[] states, double[] start_p, double[][] trans_p, double[][] emit_p)
+    {
+        double[][] V = new double[obs.length][states.length];
+        int[][] path = new int[states.length][obs.length];
+
+        for (int y : states)
+        {
+            V[0][y] = start_p[y] * emit_p[y][obs[0]];
+            path[y][0] = y;
+        }
+
+        for (int t = 1; t < obs.length; ++t)
+        {
+            int[][] newpath = new int[states.length][obs.length];
+
+            for (int y : states)
+            {
+                double prob = -1;
+                int state;
+                for (int y0 : states)
+                {
+                    double nprob = V[t - 1][y0] * trans_p[y0][y] * emit_p[y][obs[t]];
+                    if (nprob > prob)
+                    {
+                        prob = nprob;
+                        state = y0;
+                        // 记录最大概率
+                        V[t][y] = prob;
+                        // 记录路径
+                        System.arraycopy(path[state], 0, newpath[y], 0, t);
+                        newpath[y][t] = y;
+                    }
+                }
+            }
+
+            path = newpath;
+        }
+
+        double prob = -1;
+        int state = 0;
+        for (int y : states)
+        {
+            if (V[obs.length - 1][y] > prob)
+            {
+                prob = V[obs.length - 1][y];
+                state = y;
+            }
+        }
+
+        return path[state];
+    }
+}

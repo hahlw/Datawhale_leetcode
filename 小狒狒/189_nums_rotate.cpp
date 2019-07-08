2954f2e83aae5733fe40378a865edece27205f61
Bingoren <31409716+Amazingren@users.noreply.github.com>
2019-07-08 16:22:17 +0800

    Add explanation！

--- 小狒狒/189_nums_rotate.cpp
+++ 小狒狒/189_nums_rotate.cpp
@@ -11,7 +11,7 @@ public:
         if(nums.empty()) return; // 边界检查
         int len=nums.size();
         if(k>nums.size()) k = k%len;//如果要挪动的次数太多就取模，
-        vector<int>temp;创建一个临时变量
+        vector<int>temp;//创建一个临时变量
         for (int i=len-k;i<len;++i)
             temp.push_back(nums[i]);
         for (int j=0;j<len-k;++j)
@@ -19,8 +19,13 @@ public:
         nums=temp;
     }
 };
-
-
+/*
+*就创建一个临时变量，然后对于给定的K的值，那数组分成两部分；
+*第一部分：即将要移动的位数后面的所有元素，也就是将要被挤出去的元素；
+*第二部分：即将要移动的位数的前面的所有元素；
+*对第一部分，一个一个的放的新建的临时变量里，挤出去的先进入！
+*对第二部分，还在的就排在刚刚的新的临时变量里面！
+*/
 //方法二：迭代器
 class Solution {
 public:
@@ -33,4 +38,4 @@ public:
         nums.insert(nums.begin(), temp.begin(), temp.end());
     }
 };
-
+//迭代器的不是很懂，但是思想和第一种的一样！

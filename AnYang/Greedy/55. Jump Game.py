c627891d35c4ecb92d3fb90bf279a9c52b2123d2
Yangidan <anyang49@gmail.com>
2019-07-12 00:03:22 +0900

    2019-07-11

--- AnYang/Greedy/55. Jump Game.py
+++ AnYang/Greedy/55. Jump Game.py
@@ -1,4 +1,22 @@
-# 2019-07-10 Wed
+# 2019-07-11 Thu
+
+""" 97.73% """
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        if len(nums)==1 or (0 not in nums): return True
+        i = 0
+        boundary = nums[0]
+        cur = i = 0
+        for i in range(len(nums)):
+            t = i+ nums[i]
+            if boundary < t:
+                boundary = t
+                continue
+            if boundary >= len(nums)-1:
+                return True
+            if i >= boundary:
+                return False
+        return True
 
 
 """ 79.26% """

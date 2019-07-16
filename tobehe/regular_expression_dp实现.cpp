c45434d388bf6890d59749b6e5e420b834338c42
tobehe <1136779068@qq.com>
2019-07-16 08:22:50 +0800

    2015715 regular expression dp
    2015715

---
+++ tobehe/regular_expression_dp实现.cpp
@@ -0,0 +1,60 @@
+bool isMatch(string s, string p)
+{
+ 	int sdim = s.size();
+ 	int pdim = p.size();
+
+	if(pdim == 0) return sdim == 0;
+	if(pdim == 1) return (sdim == 1)&&(s[0]==p[0]||p[0] == '.');
+
+	bool **t = new bool *[pdim + 1];
+
+	for(int i = 0; i < pdim + 1; i++)
+	{
+		t[i] = new bool[sdim + 1];
+	}
+
+	t[0][0] = 1;
+
+	for(int i = 1; i < sdim + 1; i++ )
+	{
+		t[0][i] = 0;
+	}
+
+	for(int i = 1; i < pdim + 1; i++)
+	{
+		t[i][0] = p[i - 1] != '*'? false:i == 1?false:(t[i -1][0] || t[i - 2][0] );
+	}
+
+	for(int i = 1;i < pdim + 1; i++)
+	{
+		for(int j = 1; j < sdim + 1; j++)
+		{
+			if(p[i-1] == s[j -1]|| p[i-1] == '.')
+			{
+				t[i][j] = t[i - 1][j - 1];
+			}
+			else
+			{
+				if(p[i - 1] == '*')
+				{
+					t[i][j] = ((i > 1&&(s[j - 1]==p[i - 2] || p[i - 2] == '.'))&&t[i - 1][j - 1])||t[i-2][j];
+				}
+				else
+				{
+					t[i][j] = 0;
+				}
+			}
+		}
+	}
+
+	bool res = t[pdim][sdim];
+	for(int i = 0; i < pdim; i++)
+	{
+		delete t[i];
+	}
+
+	delete t;
+
+
+	return res;
+}

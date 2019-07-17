63b4bcfe23196e307a75bde27190ebb18066b03d
Hao Tian <haot@mail.smu.edu>
2019-07-17 17:49:45 +0800

    Add files via upload

---
+++ Mr.Tian/69.sqrt(x).py
@@ -0,0 +1,11 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x == 0:
+            return 0
+        
+        cur = 1
+        while True:
+            pre = cur
+            cur = (cur + x / cur) / 2
+            if abs(pre - cur) < 1e-6:
+                return int(cur)

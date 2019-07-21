d2fd8d89d638b5c83642f48f2cd681cb14ca8c2c
Hao Tian <haot@mail.smu.edu>
2019-07-21 16:22:58 +0800

    Add files via upload

---
+++ Mr.Tian/100SameTree.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def isSameTree(self, p, q):
+        if not p and not q:
+            return True
+        if not p or not q:
+            return False
+        if p.val != q.val:
+            return False
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)

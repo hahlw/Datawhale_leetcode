6652394134b1b37933dab9104d2dfbb1a2709bce
hahlw <1031087290@qqcom>
2019-07-27 19:09:57 +0800

    Update 76.最小覆盖子串.cpp

--- hahlw/string/76.最小覆盖子串.cpp
+++ hahlw/string/76.最小覆盖子串.cpp
@@ -10,33 +10,38 @@
 
 class Solution {
 public:
-    string minWindow(string s, string t) {
-        if(t.size()>s.size())return "";
+    string minWindow(string s, string t)
+    {
+        if (t.size() > s.size())
+            return "";
 
-        unordered_map<char,int>mp;
+        unordered_map<char, int> mp;
 
-        for(int i=0;i<t.size();i++){
-            if(mp.find(t[i])!=mp.end())mp[t[i]]++;
-            else mp[t[i]]=1;
+        for (int i = 0; i < t.size(); i++) {
+            if (mp.find(t[i]) != mp.end())
+                mp[t[i]]++;
+            else
+                mp[t[i]] = 1;
         }
 
-        int count=0;
-        int min_len=INT_MAX,left=0,right=0;
-        string res="";
+        int count = 0;
+        int min_len = INT_MAX, left = 0, right = 0;
+        string res = "";
 
-
-        for(;right<s.size();right++){
-            if(mp.find(s[right])!=mp.end()){
+        for (; right < s.size(); right++) {
+            if (mp.find(s[right]) != mp.end()) {
                 --mp[s[right]];
-                if(mp[s[right]]>=0)++count;
-                while(count==t.size()){
-                    if(min_len>(right-left+1)){
-                        min_len=right-left+1;
-                        res=s.substr(left,min_len);
+                if (mp[s[right]] >= 0)
+                    ++count;
+                while (count == t.size()) {
+                    if (min_len > (right - left + 1)) {
+                        min_len = right - left + 1;
+                        res = s.substr(left, min_len);
                     }
-                    if(mp.find(s[left])!=mp.end()){
+                    if (mp.find(s[left]) != mp.end()) {
                         ++mp[s[left]];
-                        if(mp[s[left]]>0)--count;
+                        if (mp[s[left]] > 0)
+                            --count;
                     }
                     ++left;
                 }
@@ -45,4 +50,3 @@ public:
         return res;
     }
 };
-

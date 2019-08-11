9a061bf11b987bab700d31655ba98a4fb24795a8
tobehe <1136779068@qq.com>
2019-08-11 15:06:21 +0800

    2019.8011
    symetric tree

---
+++ tobehe/101.symmetric-tree.cpp
@@ -0,0 +1,47 @@
+/*
+ * @lc app=leetcode id=101 lang=cpp
+ *
+ * [101] Symmetric Tree
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        return isMirror(root,root);
+    }
+
+    bool isMirror(TreeNode *p,TreeNode *q)
+    {
+        if(!p&&!q)
+        {
+            return true;
+        }
+
+        if(!p || !q)
+        {
+
+            return false;   
+        }
+        
+        if(p->val == q->val)
+        {
+            return isMirror(p->left,q->right)&&isMirror(p->right,q->left);     
+        }
+        else
+        {
+            return false;
+        }
+        
+       
+
+    }
+};
+

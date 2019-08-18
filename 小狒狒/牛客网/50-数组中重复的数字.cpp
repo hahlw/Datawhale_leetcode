1e621f01ddd715accde2cd809c0c87f51211471c
Bingoren <31409716+Amazingren@users.noreply.github.com>
2019-08-18 10:23:25 +0800

    Create 50-数组中重复的数字.cpp

---
+++ 小狒狒/牛客网/50-数组中重复的数字.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    // Parameters:
+    //        numbers:     an array of integers
+    //        length:      the length of array numbers
+    //        duplication: (Output) the duplicated number in the array number
+    // Return value:       true if the input is valid, and there are some duplications in the array number
+    //                     otherwise false
+    //还是hash map的方法平易近人！我喜欢！
+    bool duplicate(int numbers[], int length, int* duplication) {
+        map<int, int> m;
+        for(int i=0; i<length; i++){
+            m[numbers[i]]++;
+            if(m[numbers[i]] > 1){
+                *duplication = numbers[i];
+                return true;
+            }
+        }
+        return false;
+    }
+};

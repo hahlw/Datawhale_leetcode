9ca23ba0ba30e5052cbb36d3f1a2e1029d35c0f9
LittleBadGirl <52447463+LittleBadGirl@users.noreply.github.com>
2019-09-07 23:16:57 +0800

    Create 94.cpp

---
+++ 小脑袋/94.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+ //递归
+class Solution {
+    vector<int> ans;
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        if(root) {
+            inorderTraversal(root->left);
+            ans.push_back(root->val);
+            inorderTraversal(root->right);
+        }
+        return ans;
+    }
+};

11c0b7e667e8547de0fe0072654e8b5a5b8e7d78
shawn <sdd031215@163.com>
2019-09-18 08:26:43 +0800

    Update ZConvert.java

--- shawn/ZConvert.java
+++ shawn/ZConvert.java
@@ -23,6 +23,55 @@ class Solution {
         for (StringBuilder row : rows) ret.append(row);
         return ret.toString();
     }
-}
 
-//按列排序
+
+    //按行排序2
+    //行 0 中的字符位于索引 k  = (2 * numRows  - 2) 处;
+    //行 numRows-1 中的字符位于索引 k = (2⋅numRows−2)+numRows−1 处;
+    //内部的 行 i 中的字符位于索引 k=(2⋅numRows−2)+i 以及 (k+1)=(2⋅numRows−2)−i 处;
+    public String convert(String s, int numRows) {
+
+        if (numRows == 1) return s;
+
+        StringBuilder ret = new StringBuilder();
+        int n = s.length();
+        int cycleLen = 2 * numRows - 2;
+
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j + i < n; j += cycleLen) {
+                ret.append(s.charAt(j + i));
+                if (i != 0 && i != numRows - 1 && j + cycleLen - i < n)
+                    ret.append(s.charAt(j + cycleLen - i));
+            }
+        }
+        return ret.toString();
+    }
+
+    //技巧
+    public String convert3(String s, int numRows) {
+
+        if(numRows == 1) return s;
+        //存储每一行下一次的插入下标
+        int[] rowIdx = new int[numRows];
+        char[] chars = new char[s.length()];
+        int n = 0;
+        int burketSize = numRows * 2 - 2;
+        int burketNum = chars.length / burketSize; 
+        int rem = chars.length % burketSize;
+        for(int i = 1; i < numRows; i ++){
+        	int flag = i == 1 ? 1 : 2;
+        	n = flag * burketNum + (rem >= i ? ( 1 + (burketSize - rem + 1 < i ? 1 : 0)) : 0);
+        	rowIdx[i] = rowIdx[i-1] + n;
+        }
+        int flag = -1;
+        int curRow = 0;
+        for(char c : s.toCharArray()){
+        	chars[rowIdx[curRow]] = c;
+        	rowIdx[curRow] = rowIdx[curRow] + 1;
+        	 if (curRow == 0 || curRow == numRows - 1) flag = -flag;
+             curRow += flag;
+        }
+        return new String(chars);
+    }
+    
+}

f7349ed786ba10559436a3120823803e55928541
shawn <sdd031215@163.com>
2019-09-21 11:02:32 +0800

    Create SwapPairs.java

---
+++ shawn/SwapPairs.java
@@ -0,0 +1,31 @@
+//两两交换链表中的节点
+
+class Solution {
+    //递归解法
+    public ListNode swapPairs(ListNode head) {
+        if(head == null || head.next == null){
+            return head;
+        }
+        ListNode next = head.next;
+        head.next = swapPairs(next.next);
+        next.next = head;
+        return next;
+    }
+    //非递归解法
+    public ListNode swapPairs2(ListNode head) {
+        ListNode pre = new ListNode(0);
+        pre.next = head;
+        ListNode temp = pre;
+        while(temp.next != null && temp.next.next != null) {
+            ListNode start = temp.next;
+            ListNode end = temp.next.next;
+            temp.next = end;
+            start.next = end.next;
+            end.next = start;
+            temp = start;
+        }
+        return pre.next;
+    }
+
+
+}

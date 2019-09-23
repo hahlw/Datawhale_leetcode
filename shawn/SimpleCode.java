c7f625856a74a12ac04498b858418119a8c4b1d5
shawn <sdd031215@163.com>
2019-09-23 22:31:48 +0800

    Update SimpleCode.java

--- shawn/SimpleCode.java
+++ shawn/SimpleCode.java
@@ -9,4 +9,22 @@ class Solution {
         }
         return j;
     }
+    
+    public int removeElement2(int[] nums, int val) {
+        int i = 0;
+        int n = nums.length;
+        while (i < n) {
+            if (nums[i] == val) {
+                nums[i] = nums[n - 1];
+                // reduce array size by one
+                n--;
+            } else {
+                i++;
+            }
+        }
+        return n;
+    }
+    
+    
+
 }

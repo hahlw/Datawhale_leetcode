2b43a8d962db28a94e8630d80c1197ac83d931e7
shawn <sdd031215@163.com>
2019-09-25 09:25:50 +0800

    Update SimpleCode.java

--- shawn/SimpleCode.java
+++ shawn/SimpleCode.java
@@ -1,4 +1,5 @@
 class Solution {
+    //从数组中移走指定数字
     public int removeElement(int[] nums, int val) {
         int j = 0;
         for ( int i = 0; i < nums.length; i++) {
@@ -25,6 +26,23 @@ class Solution {
         return n;
     }
     
+    //一个字符串在另外一个字符串中出现的位置
+    int search(String pat, String txt) {
+        int M = pat.length;
+        int N = txt.length;
+        for (int i = 0; i < N - M; i++) {
+            int j;
+            for (j = 0; j < M; j++) {
+                if (pat[j] != txt[i+j])
+                    break;
+            }
+            // pat 全都匹配了
+            if (j == M) return i;
+        }
+        // txt 中不存在 pat 子串
+        return -1;
+    }
+
     
 
 }

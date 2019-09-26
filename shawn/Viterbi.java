1c0c1454b6046700e4798a654fc1b3e12f3bc38c
shawn <sdd031215@163.com>
2019-09-26 22:06:58 +0800

    Update Viterbi.java

--- shawn/Viterbi.java
+++ shawn/Viterbi.java
@@ -165,3 +165,45 @@ public class Viterbi
         return path[state];
     }
 }
+
+/*
+def viterbi(x, pi, A, B):
+    """
+    x: user input string/sentence: x: "I like playing soccer"
+    pi: initial probability of tags
+    A: 给定tag, 每个单词出现的概率
+    B: tag之间的转移概率
+    """
+    x = [word2id[word] for word in x.split(" ")]  # x: [4521, 412, 542 ..]
+    T = len(x)
+    
+    dp = np.zeros((T,N))  # dp[i][j]: w1...wi, 假设wi的tag是第j个tag
+    ptr = np.array([[0 for x in range(N)] for y in range(T)] ) # T*N
+    # TODO: ptr = np.zeros((T,N), dtype=int)
+    
+    for j in range(N): # basecase for DP算法
+        dp[0][j] = log(pi[j]) + log(A[j][x[0]])
+    
+    for i in range(1,T): # 每个单词
+        for j in range(N):  # 每个词性
+            # TODO: 以下几行代码可以写成一行（vectorize的操作， 会使得效率变高）
+            dp[i][j] = -9999999
+            for k in range(N): # 从每一个k可以到达j
+                score = dp[i-1][k] + log(B[k][j]) + log(A[j][x[i]])
+                if score > dp[i][j]:
+                    dp[i][j] = score
+                    ptr[i][j] = k
+    
+    # decoding: 把最好的tag sequence 打印出来
+    best_seq = [0]*T  # best_seq = [1,5,2,23,4,...]  
+    # step1: 找出对应于最后一个单词的词性
+    best_seq[T-1] = np.argmax(dp[T-1])
+    
+    # step2: 通过从后到前的循环来依次求出每个单词的词性
+    for i in range(T-2, -1, -1): # T-2, T-1,... 1, 0
+        best_seq[i] = ptr[i+1][best_seq[i+1]]
+        
+    # 到目前为止, best_seq存放了对应于x的 词性序列
+    for i in range(len(best_seq)):
+        print (id2tag[best_seq[i]])
+ */

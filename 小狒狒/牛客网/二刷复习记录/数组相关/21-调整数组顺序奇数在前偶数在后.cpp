3f7c7bb49f0570e073ddd282e9e3a2bc061c0307
Bingoren <31409716+Amazingren@users.noreply.github.com>
2019-09-29 17:40:16 +0800

    Create 21-调整数组顺序奇数在前偶数在后.cpp

---
+++ 小狒狒/牛客网/二刷复习记录/数组相关/21-调整数组顺序奇数在前偶数在后.cpp
@@ -0,0 +1,17 @@
+//这个有点投机取巧了
+class Solution {
+public:
+    void reOrderArray(vector<int> &array) {
+        vector<int> res;
+        //if(array.empty()) return NULL;
+        for(int i= 0; i < array.size(); i++){
+            if(array[i] % 2 == 1)
+                res.push_back(array[i]);
+        }
+        for(int i=0; i < array.size(); i++){
+            if(array[i] % 2 == 0)
+                res.push_back(array[i]);
+        }
+        array = res;
+    }
+};

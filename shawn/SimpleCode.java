be7a94fddfb097c4150e0d46bb58bf3890994ad0
shawn <sdd031215@163.com>
2019-09-29 09:31:56 +0800

    Update SimpleCode.java

--- shawn/SimpleCode.java
+++ shawn/SimpleCode.java
@@ -43,6 +43,36 @@ class Solution {
         return -1;
     }
 
-    
+    public int romanToInt(String s) {
+        if(s == null || s.length() ==0) return 0;
+        Map<String, Integer> map = new HashMap<>();
+        map.put("I", 1);
+        map.put("IV", 4);
+        map.put("V", 5);
+        map.put("IX", 9);
+        map.put("X", 10);
+        map.put("XL", 40);
+        map.put("L", 50);
+        map.put("XC", 90);
+        map.put("C", 100);
+        map.put("CD", 400);
+        map.put("D", 500);
+        map.put("CM", 900);
+        map.put("M", 1000);
+        
+        int sum = 0;
+        // char[] arr = s.toCharArray();
+        for(int i = 0; i<s.length();){
+            if(i + 1 < s.length() && map.containsKey(s.substring(i, i+2))) {
+                sum += map.get(s.substring(i, i+2));
+                i += 2;
+            } else {
+                sum += map.get(s.substring(i, i+1));
+                i ++;
+            }
+        }
+               
+        return sum;
+    }
 
 }

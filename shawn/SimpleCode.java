566a20ddaf079898671309bcc71ee32375609058
shawn <sdd031215@163.com>
2019-10-20 22:55:10 +0800

    Update SimpleCode.java

--- shawn/SimpleCode.java
+++ shawn/SimpleCode.java
@@ -228,4 +228,32 @@ class Solution {
 		return temp;
     }
     
+    //38.报数
+    public static String countAndSay(int n) {
+        StringBuilder ans = new StringBuilder();
+        ans.append("1");
+        for (int i = 2; i <= n ; i++) {
+            //遍历前一个字符串
+            String currentStr = new String(ans);
+            ans.delete(0,ans.length());
+            int num = 0;
+            char currentChar = currentStr.charAt(0);
+            for (char c : currentStr.toCharArray()) {
+                if(c == currentChar){
+                    num++;
+                }
+                else{
+                    ans.append((char)('0'+num));
+                    ans.append(currentChar);
+                    currentChar = c;
+                    num = 1;
+                }
+            }
+            ans.append((char)('0'+num));
+            ans.append(currentChar);
+
+        }
+
+        return ans.toString();
+    }
 }
